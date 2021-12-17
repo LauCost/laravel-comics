95cbec358feef4236619b826e8bcb103d3b72bd1
Laurentiu <lcostin40@gmail.com>
2021-12-17 17:57:26 +0100

    Aggiunto footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,73 @@
+<footer>
+
+    <!-- Links -->
+    <div class="background">
+    <div class="cont links flex">
+      <div class="col_1">
+        <h3>DC COMICS</h3>
+
+        <ul>
+          <li>Charcaters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
+        </ul>
+
+        <h3>SHOP</h3>
+
+        <ul>
+          <li>Shop DC</li>
+          <li>Shop DC Collectibles</li>
+        </ul>
+      </div>
+
+      <div class="col_2">
+        <h3>DC</h3>
+        <ul>
+          <li>Terms Of Use</li>
+          <li>Privacy policy (New)</li>
+          <li>Ad Choices</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certificates</li>
+          <li>Ratings</li>
+          <li>Shops Help</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div class="col_3">
+        <h3>SITES</h3>
+        <ul>
+          <li>DC</li>
+          <li>MAD Magazine</li>
+          <li>DC Kids</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+    <!-- Follow -->
+    <div class="black">
+    <div class="cont dispi">
+      <button class="sig">SIGN-UP NOW!</button>
+      <div class="social flex">
+        <p>FOLLOW US</p>
+        <img src="{{asset('images/footer-facebook.png')}}" alt="" />
+        <img src="{{asset('images/footer-twitter.png')}}" alt="" />
+        <img src="{{asset('images/footer-youtube.png')}}" alt="" />
+        <img src="{{asset('images/footer-pinterest.png')}}" alt="" />
+        <img src="{{asset('images/footer-periscope.png')}}" alt="" />
+      </div>
+    </div>
+
+  </div>
+  </footer>
