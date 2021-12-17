5fe37c2ec4bb509a8b3cde9994c640e4302f18cf
Laurentiu <lcostin40@gmail.com>
2021-12-17 18:57:21 +0100

    Aggiunto card dei fumetti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,20 @@
 @section('main')
 
 
-<div class="container">
-    <h1>TEST</h1>
-    <p>TEST</p>
+<div class="jumbo">
+      <p class="series">CURRENT SERIES</p>
 </div>
+
+    <div class="cont flex_wrap">
+        @foreach ($comics as $comic)
+        <div class="card">
+            <img src="{{$comic['thumb']}}" alt="" />
+            <p>{{ $comic['title'] }}</p>
+        </div>
+        @endforeach
+        <button class="load_series">LOAD MORE</button>
+    </div>
+    <!-- <Utility /> -->
 
 
 @endsection
