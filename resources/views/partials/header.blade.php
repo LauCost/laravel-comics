65953fa6e924abe24054077ce75cae5a53bcb325
Laurentiu <lcostin40@gmail.com>
2021-12-17 16:41:22 +0100

    Creata struttura non funzionante

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+
+    <div class="cont_img_nav">
+
+      <!-- <img src="../assets/img/dc-logo.png" alt="" /> -->
+
+      <nav>
+        <a href="#">CHARACTERS</a>
+        <a class="attivo" href="#">COMICS</a>
+        <a href="#">MOVIES</a>
+        <a href="#">TV</a>
+        <a href="#">GAMES</a>
+        <a href="#">COLLECTIBLES</a>
+        <a href="#">VIDEOS</a>
+        <a href="#">FANS</a>
+        <a href="#">NEWS</a>
+        <a href="#">SHOP</a>
+      </nav>
+
+    </div>
+
+  </header>
